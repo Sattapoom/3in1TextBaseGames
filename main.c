002c2273fdc877a6276990c3d096a73ab93a9dc8
Sattapoom Tulyasuk <s6301012630192@kmutnb.ac.th>
2021-12-04 15:09:00 +0700

    Delete & Optimize all useless function

--- main.c
+++ main.c
@@ -8,121 +8,107 @@
 
 // =========================== MAIN MENU ====================================
 
-COORD coord= {0,0}; 
-void gotoxy(int x,int y)
-{
-    coord.X=x;
-    coord.Y=y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
-}
+    // ====================== Golbal Variable =======================
 
-
-typedef struct
-{
-    int line,line2,name;
-	int sum,percen,bus;
-	
-    
-} Data;
-
-// ========== Golbal Variable =================
-void DisplayMainmenu();
-void DisplayEndgamemenu();
-void Curser();
-void Endgamecurser(int);
-void highlight(int);
-void window(int a,int b,int c,int d);
+void DisplayMenu();
+void Highlight(int,int);
 
 void Game1();
 void Game2();
 void Game3();
+void stopgame(int,char*);
 
-int currentgame;
-int menunum;
-// EOC ====== Golbal Variable ====== EOC ======
+COORD pos= {0,0}; 
+int currentwindow;
+int lastgame;
+
+    // EOC ============ Golbal Variable ============ EOC ============
 
 int main()
 {
-    DisplayMainmenu();
+    currentwindow = 0;
+    DisplayMenu();
     return 0;
 }
 
-
-void DisplayMainmenu()
+void changepos(int x,int y)
 {
-	int i;
-    const char *menu[]= {"   Game 1 ","   Game 2 ","   Game 3 ","   Exit"};
-    system("cls");
-	window(25,50,20,30);
-    gotoxy(33,18);
-    printf("Main Menu");
-   for (i=0; i<=3; i++)
-  {
-        gotoxy(30,23+i);
-       printf("%s\n\n\n",menu[i]);
-  }
-    menunum = 4;
-    Curser();
+    pos.X=x;
+    pos.Y=y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),pos);
 }
 
-
-void DisplayEndgamemenu()
+void DisplayMenu()
 {
-    int i;
-    const char *menu[]= {"   Play Again ","   Main Menu "};
     system("cls");
-	window(25,50,20,28);
-    gotoxy(33,18);
-    printf("End Game");
-   for (i=0; i<=1; i++)
-  {
-        gotoxy(30,23+i);
-       printf("%s\n\n\n",menu[i]);
-  }
-    menunum = 2;
-    Curser();
-}
-
-
-void Curser()
-{
-    int count=1;
+    int totalchoice;
+    char *menu[4];
+    if(currentwindow == 0)
+    {
+        menu[0]= "  Game 1 ";
+        menu[1]= "  Game 2 ";
+        menu[2]= "  Game 3 ";
+        menu[3]= "  Exit";
+        totalchoice=4;
+        changepos(0,1);
+        printf("*********** Main Menu ***********");
+    }
+    else if(currentwindow == -1)
+    {
+        menu[0]= "   Play Again ";
+        menu[1]= "   Main Menu ";
+        totalchoice=2;
+        changepos(0,1);
+        printf("*********** Game Over ***********");
+    }
+    for (int i=0; i<totalchoice; i++)
+    {
+        changepos(5,3+i);
+        printf("%s\n\n\n",menu[i]);
+    }
+    
+    
+    int selectedchioce=1;
     char ch='0';
-    gotoxy(30,23);
+    changepos(5,3);
     while(1)
     {
         switch(ch)
         {
         case 80:
-            count++;
-            if (count==menunum+1) count=1;
+            selectedchioce++;
+            if (selectedchioce==totalchoice+1) selectedchioce=1;
             break;
         case 72:
-            count--;
-            if(count==0) count=menunum;
+            selectedchioce--;
+            if(selectedchioce==0) selectedchioce=totalchoice;
             break;
         }
-        highlight(count);
+        Highlight(selectedchioce, totalchoice);
         ch=getch();
         if(ch=='\r')
         {
             system("cls");
-           if(menunum==4)
+           if(currentwindow == 0)
             {
-                if (count==1) Game1();
-                else if(count==2) Game2();
-                else if(count==3) Game3();
+                if (selectedchioce==1) Game1();
+                else if(selectedchioce==2) Game2();
+                else if(selectedchioce==3) Game3();
                 else   exit(0);
             }
-            if(menunum==2)
+            if(currentwindow == -1)
             {   
-                if (count==1)
+                if (selectedchioce==1)
                 {
-                    if (currentgame == 1) Game1();
-                    if (currentgame == 2) Game2();
-                    if (currentgame == 3) Game3();
+                    if (lastgame == 1) Game1();
+                    else if (lastgame == 2) Game2();
+                    else if (lastgame == 3) Game3();
                 }
-                else if(count==2) DisplayMainmenu();
+                else if(selectedchioce == 2) 
+                {
+                    currentwindow = 0;
+                    DisplayMenu();
+                }
                 else   exit(0);
             }
             break;
@@ -131,140 +117,115 @@ void Curser()
 }
 
 
-
-void highlight(int count)
+void Highlight(int count, int totalchoice)
 {
-    if(menunum == 4)
+    if(totalchoice == 4)
     {
-        gotoxy (30,23);
+        changepos (5,3);
         printf("   Game 1 ");
-        gotoxy (30,24);
+        changepos (5,4);
         printf("   Game 2 ");
-        gotoxy (30,25);
+        changepos (5,5);
         printf("   Game 3 ");
-        gotoxy (30,26);
+        changepos (5,6);
         printf("   Exit        ");
         switch(count)
         {
         case 1:
-            gotoxy (30,23);
-            printf(" - Game 1 ");
+            changepos (5,3);
+            printf("- Game 1 ");
             break;
         case 2:
-            gotoxy (30,24);
-            printf(" - Game 2 ");
+            changepos (5,4);
+            printf("- Game 2 ");
             break;
         case 3:
-            gotoxy (30,25);
-            printf(" - Game 3 ");
+            changepos (5,5);
+            printf("- Game 3 ");
             break;
         case 4:
-            gotoxy (30,26);
-            printf(" - Exit ");
+            changepos (5,6);
+            printf("- Exit ");
             break;
         }
     }
-    else if(menunum == 2)
+    else if(totalchoice == 2)
     {
-        gotoxy (30,23);
+        changepos (5,3);
         printf("   Play Again ");
-        gotoxy (30,24);
+        changepos (5,4);
         printf("   Main Menu ");
     
         switch(count)
         {
         case 1:
-            gotoxy (30,23);
-            printf(" - Play Again ");
+            changepos (5,3);
+            printf("- Play Again ");
             break;
         case 2:
-            gotoxy (30,24);
-            printf(" - Main Menu ");
+            changepos (5,4);
+            printf("- Main Menu ");
             break;
         }
     }
 }
 
-
-void window(int a,int b,int c,int d)
-{
-    int i;
-    system("cls");
-    gotoxy(20,10);
-    for (i=1; i<=11; i++)
-        printf("=");
-    printf(" Play Games ");
-    for (i=1; i<=11; i++)
-        printf("=");
-    printf("\n\n");
-    gotoxy(21,11);
-    printf("\t    Choose Your Games");
-    for (i=a; i<=b; i++)
-    {
-        gotoxy(i,17);
-        printf("=");
-        gotoxy(i,19);
-        printf("=");
-        gotoxy(i,c);
-        printf(".");
-        gotoxy(i,d);
-        printf(".");
-    }
-    gotoxy(a,17);
-    printf("=");
-    gotoxy(a,19);
-    printf("=");
-    gotoxy(b,17);
-    printf("=");
-    gotoxy(b,19);
-    printf("=");
-    for(i=c; i<=d; i++)
-    {
-        gotoxy(a,i);
-        printf(".");
-        gotoxy(b,i);
-        printf(".");
-    }
-    gotoxy(a,c);
-    printf(".");
-    gotoxy(a,d);
-    printf(".");
-    gotoxy(b,c);
-    printf(".");
-    gotoxy(b,d);
-    printf(".");
-}
 // ===========EOC============ MAIN MENU ===============EOC==================
+
+
+
+// ============================= GAME ======================================
+void stopgame(int lgame,char* status)
+{   
+    changepos(0,0);
+    printf("- - - YOU %s - - -",status);
+    lastgame = lgame;
+    if(getch() == '\r')
+    {
+        currentwindow = -1;
+        DisplayMenu();
+    }
+}
+
 
 void Game1()
 {
-    currentgame =1;
+    currentwindow = 1;
+
+    // Add game 1 here vvvvv
+    changepos(0,1);
 	printf("Playing 1'st game.\n\n\n");
-    
-    if(getch() == '\r')
-    {
-        DisplayEndgamemenu();
-    }
+
+    // Add game 1 here ^^^^^
+
+    stopgame(1,"WIN");
 }
 
 
 void Game2()
 {
-    currentgame = 2;
+    currentwindow = 2;
+
+    // Add game 2 here vvvvv
+    changepos(0,1);
 	printf("Playing 2'nd game.\n\n\n");
-    if(getch() == '\r')
-    {
-        DisplayEndgamemenu();
-    }
+    
+    // Add game 2 here ^^^^^
+
+    stopgame(2,"WIN");
 }
 
 
 void Game3()
 {
-    currentgame = 3;
+    currentwindow = 3;
+
+    // Add game 3 here vvvvv
+    changepos(0,1);
 	printf("Playing 3'rd game.\n\n\n");
-    if(getch() == '\r')
-    {
-        DisplayEndgamemenu();
-    }
+    
+    // Add game 3 here ^^^^^
+
+    stopgame(3,"WIN");
 }
+// ========= EOC =============== GAME ============== EOC ===================
